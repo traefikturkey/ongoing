828c3804db2a5b083cf5c6978f73e650687fbfd6
mike <219478+ilude@users.noreply.github.com>
2024-03-22 12:57:23 -0400

    detect docker socket and update perms

--- .devcontainer/entrypoint.d/docker-socket-perms.sh
+++ .devcontainer/entrypoint.d/docker-socket-perms.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo chown ${USER}:${USER} /var/run/docker.sock
+
+# Check if /var/run/docker.sock exists
+if [ -S "/var/run/docker.sock" ]; then
+    echo "Docker socket detected. changing ownership to  ${USER}:${USER}..."
+    sudo chown ${USER}:${USER} /var/run/docker.sock
+fi

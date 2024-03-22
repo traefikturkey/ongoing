ba6c769238838fb7588151d372718f923860402e
mike <219478+ilude@users.noreply.github.com>
2024-03-22 14:02:59 -0400

    fix flask debugging and interface binding

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 from selenium import webdriver
@@ -39,4 +40,4 @@ def index():
     return render_template('index.html', url=url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=os.environ.get("FLASK_DEBUG", "False"))

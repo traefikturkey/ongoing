dd13e2ed554a98322360110d5af5585a54e49429
mike <219478+ilude@users.noreply.github.com>
2024-03-25 12:25:05 -0400

    lets see what this breaks

--- app/app.py
+++ app/app.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 
 # Create a headless browser instance
 options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
 options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--profile-directory=Default')
+options.add_argument('--user-data-dir=~/.config/google-chrome')
 driver = webdriver.Chrome(options=options)
 
 def expand_url(url):
